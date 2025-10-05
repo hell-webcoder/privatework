94a1e0f2d08bc7c7285b7558efcbc67238d07d1b
esterwilly <215452058+hell-webcoder@users.noreply.github.com>
2025-10-05 12:18:34 +0500

    push

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const path = "./data.json";
 
 const markCommit = (x, y) => {
   const date = moment()
-    .subtract(1, "y")
-    .add(1, "d")
+    .year(2025)
+    .startOf('year')
     .add(x, "w")
     .add(y, "d")
     .format();
@@ -26,7 +26,7 @@ const makeCommits = (n) => {
   if(n===0) return simpleGit().push();
   const x = random.int(0, 54);
   const y = random.int(0, 6);
-  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+  const date = moment().year(2025).startOf('year').add(x, "w").add(y, "d").format();
 
   const data = {
     date: date,
@@ -37,4 +37,4 @@ const makeCommits = (n) => {
   });
 };
 
-makeCommits(10);
+makeCommits(2000);
