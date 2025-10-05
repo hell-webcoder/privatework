ad26a4960a182c14c4627c1462e82d799e85e554
esterwilly <215452058+hell-webcoder@users.noreply.github.com>
2025-10-05 12:26:09 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import random from "random";
 const path = "./data.json";
 
 const markCommit = (x, y) => {
-  const randomDay = random.int(0, 364); // 365 days in 2025 (0-364)
+  const randomDay = random.int(0, 272); // 273 days from Jan 1 to Sep 30 (0-272)
   const date = moment()
     .year(2025)
     .startOf('year')
@@ -24,7 +24,7 @@ const markCommit = (x, y) => {
 
 const makeCommits = (n) => {
   if(n===0) return simpleGit().push();
-  const randomDay = random.int(0, 364); // 365 days in 2025 (0-364)
+  const randomDay = random.int(0, 272); // 273 days from Jan 1 to Sep 30 (0-272)
   const date = moment().year(2025).startOf('year').add(randomDay, "d").format();
 
   const data = {
